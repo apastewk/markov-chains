131afffb7b95ed18498092a0b5a53c17c343fd0d
Hackbright Student <info@hackbrightacademy.com>
2016-07-13 12:32:22 -0700

    Uses sys.argv, begins with capital letter and ends with anything that's not alphabet, takes in two text files and merges them into one dictionary.

--- markov.py
+++ markov.py
@@ -1,4 +1,5 @@
 from random import choice
+import sys
 
 
 def open_and_read_file(file_path):
@@ -12,7 +13,7 @@ def open_and_read_file(file_path):
     return text_file
 
 
-def make_chains(text_string):
+def make_chains(text_string, text_string2):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -25,18 +26,35 @@ def make_chains(text_string):
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
 
+    
     chains = {}
-    words = text_string.split()
 
-    for i in range(len(words) - 2):
-        key_words = words[i], words[i + 1]
-        value_list = words[i + 2]
+    def populate_chain(string_text):
+        words = text_string.split()
+        for i in range(len(words) - 2):
+            key_words = words[i], words[i + 1]
+            value_list = words[i + 2]
 
-        if not chains.get(key_words):
-            chains[key_words] = [value_list]
-        else:
-            chains.get(key_words).append(value_list)
-    # your code goes here
+            if not chains.get(key_words):
+                chains[key_words] = [value_list]
+            else:
+                chains.get(key_words).append(value_list)
+        return chains
+
+    populate_chain(text_string)
+    populate_chain(text_string2)
+    # words = text_string2.split()   
+
+    # for i in range(len(words) - 2):
+    #     key_words = words[i], words[i + 1]
+    #     value_list = words[i + 2]
+
+    #     if not chains.get(key_words):
+    #         chains[key_words] = [value_list]
+    #     else:
+    #         chains.get(key_words).append(value_list)
+
+    # print chains
 
     return chains
 
@@ -45,15 +63,17 @@ def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
     text = ""
-    new_key = ()
     # got first random tuple. output ('','')
-    random_key = choice(chains.keys()) 
-    new_key = random_key
-    # unpack random_key
-    thing1, thing2 = random_key
-    # added tuple to text string 
-    text = thing1 + " " + thing2
- 
+    while True:
+        random_key = choice(chains.keys()) 
+        new_key = random_key
+        # unpack random_key
+        thing1, thing2 = random_key
+        # added tuple to text string 
+        if thing1[0].isupper():
+            text = thing1 + " " + thing2
+            break
+        
     while True:
         # get random_key's corresponding value as a string
         random_value = choice(chains.get(new_key))
@@ -65,17 +85,25 @@ def make_text(chains):
   
         # if the value of the new key is not in dictionary then break
         if chains.get(new_key) is None:
-            break
+            lorax, fox = new_key
+            last_char = fox[-1]
+            if last_char.isalpha() is False:
+                break
     return text
 
 
-input_path = "green-eggs.txt"
+# input_path = "gettysburg.txt"
+# input_path2 = "green-eggs.txt"
+input_path = sys.argv[1]
+input_path2 = sys.argv[2]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
+input_text2 = open_and_read_file(input_path2)
+
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, input_text2)
 
 # Produce random text
 random_text = make_text(chains)
